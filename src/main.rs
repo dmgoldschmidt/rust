b4a07a2df65a1139f840c7981a1ee61af40d77af
David <david@M2-Macbook-Pro.local>
2024-01-27 15:34:35 -0800

    Fixed a few bugs.  Lots of debugging prints remain.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::env;
-use std::process::exit;
+//use std::process::exit;
 
 fn main(){
     let options : &mut Vec<String> = &mut Vec::new();
     let values : &mut Vec<String> = &mut Vec::new();
     get_opt(options,values);
-    assert!(options.len() == values.len());
-    println!("options.len() = {}",options.len()); 
+    //assert!(options.len() == values.len());
+    println!("options.len() = {}, values.len() = {}",options.len(), values.len()); 
     for i in  1..options.len(){
         println!("option[{i}] = {:?}, value[{i}] = {:?} ",options[i], values[i]);
     }
@@ -18,42 +18,44 @@ fn get_opt(options : &mut Vec<String>, values : &mut Vec<String>) {
     let mut last_was_opt:bool = false; // parser state
     for f0 in args {
         let  my_field = &f0;
+        println!("processing {my_field}");
         let f: Vec<char> = my_field.chars().collect();
-        if f[0] == '-' && (!f[1].is_ascii_digit()) && f[1] != '.' && f[1] != '\0'  {
-            if last_was_opt {// two options in a row so there's an empty value
-                values.push("".to_string());
-            }
+        if f[0] == '-' {
             if my_field.len() == 1 { // single minus is a value with no option
-                 options.push("".to_string()); // empty option
-                 values.push("-".to_string());
-                 break;
+                options.push("".to_string()); // empty option
+                values.push("-".to_string());
+                last_was_opt=false;
+                println!("field {my_field} is a value");
+                continue;
+            }
+            if f[1].is_ascii_digit() || f[1] == '.' && f[1] == '\0' { // it's a negative number
+                values.push(my_field.to_string());
+                if !last_was_opt { options.push("".to_string()) }; // two values in a row
+                last_was_opt = false;
+                println!("field {my_field} is a value");
+                continue;
             }
             println!("field {my_field} is an option");
+ 
+            if last_was_opt {// two options in a row so there's an empty value
+                values.push("".to_string());
+                last_was_opt = false;
+            }
             if f[1] == '-'{  // double minus means long option
                 match my_field.find('='){
-                    Some(i) => {
+                    Some(i) => { // field is option=value
                         println!("found = at index {}",i);
                         options.push(f[2..i].iter().collect());
                         values.push(f[i+1..].iter().collect());
                         last_was_opt = false;
                     }
-                    None => {
+                    None => { // its a long option
                         println!("no = found");
                         options.push(f[2..].iter().collect());
+                        values.push("".to_string());
                         last_was_opt = true;
                     }
                 }
-/*
-                for i in 2..f.len() {
-                    if f[i] == b'='  { // embedded '=' in long option
-                        options.push(String::from_utf8(f[2..i]));
-                        values.push(String{f:f[i+1..]});
-                        last_was_opt = false;
-                        break;
-                    }
-                }
-                options.push(String(f[2..]));
-                 */
             }
             else {options.push(f[1..].iter().collect());
                   last_was_opt = true;
@@ -65,6 +67,7 @@ fn get_opt(options : &mut Vec<String>, values : &mut Vec<String>) {
             }
             values.push(my_field.to_string());
             last_was_opt = false;
+            println!("field {my_field} is a value");
         }
     }
 }
